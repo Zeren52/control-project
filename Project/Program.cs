d4f02d9348103e4af6d1cf2ea7283326624f1692
Zeren52 <153220344+Zeren52@users.noreply.github.com>
2024-05-20 09:37:46 +0300

    Add files via upload

---
+++ Project/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] array1 = new string[6] {"start", "25", "367", "cater", "big","466"};
+string[] SecondArray(string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; ++i)
+    {
+    if(array1[i].Length <= 3)
+        {
+        ++count;
+        }
+    }
+    string[] res = new string[count];
+    int j = 0;
+    for (int i = 0; i < array1.Length; ++i)
+    {
+    if(array1[i].Length <= 3)
+        {
+        res[j++] = array1[i];
+        }
+    }
+    return res;
+}   
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; ++i)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+string[] array2 = SecondArray(array1);
+PrintArray(array1);
+PrintArray(array2);
